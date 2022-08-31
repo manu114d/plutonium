354cba5c166737abae03e7b9efa57747442cf854
Manu <manu114d@gmail.com>
2022-08-31 12:42:01 +0530

    updated

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,17 +4,17 @@ const tokenAuthenticator = function(req,res,next){
     let reqHeaders=req.headers;
     if(reqHeaders["x-auth-token"])
     {
-        let token=reqHeaders["x-auth-token"];
-        let decodedToken=jwt.verify(token,'SecretKey');
-        if(req.params.userId==decodedToken._id)
-        {
+        // let token=reqHeaders["x-auth-token"];
+        // let decodedToken=jwt.verify(token,'SecretKey');
+        // if(req.params.userId==decodedToken._id)
+        // {
             next();
         }
-        else
-        {
-            res.send({status : false,msg : 'Invalid Token!'});
-        }
-    }
+    //     else
+    //     {
+    //         res.send({status : false,msg : 'Invalid Token!'});
+    //     }
+    // }
     else
     {
         res.send({status : false,msg : "Request is missing a mandatory HEADER!"});
@@ -22,3 +22,4 @@ const tokenAuthenticator = function(req,res,next){
 };
 
 module.exports={tokenAuthenticator};
+
